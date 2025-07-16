ee03242a35bc00587ab10d89411a79c2ff215f88
Franco D'Angelo <100707867+franciscdaydream@users.noreply.github.com>
2025-07-16 14:19:30 -0300

    Update cuadrados.js

--- cuadrados.js
+++ cuadrados.js
@@ -7,20 +7,10 @@ class cuadros {
   }
 
   dibujar() {
-    let angulo = 0;
-    if (cambioLugar < 50) {
-      angulo = 0;
-    } else if (cambioLugar < 100) {
-      angulo = HALF_PI;
-    } else if (cambioLugar < 150) {
-      angulo = PI;
-    } else {
-      angulo = PI + HALF_PI;
-    }
-
+    
     push();
     translate(this.x + anchocuadros / 2, this.y + anchocuadros / 2);
-    rotate(angulo);
+
     imageMode(CENTER);
     image(mosaicos0[this.cambio], 0, 0, anchocuadros, anchocuadros);
     pop();
