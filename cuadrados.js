bc2879ae44ba1f6e24dfaf857d6645444befa00e
Franco D'Angelo <100707867+franciscdaydream@users.noreply.github.com>
2025-07-16 14:55:25 -0300

    los cuadrados ya no giran rarosa

--- cuadrados.js
+++ cuadrados.js
@@ -10,7 +10,6 @@ class cuadros {
     
     push();
     translate(this.x + anchocuadros / 2, this.y + anchocuadros / 2);
-
     imageMode(CENTER);
     image(mosaicos0[this.cambio], 0, 0, anchocuadros, anchocuadros);
     pop();
